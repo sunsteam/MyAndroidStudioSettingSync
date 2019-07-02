63842beed17ddc371adbc4d08c2329e0445427af
Y.X.Sun <sunsteam@163.com>
2019-07-02 10:02:48 +0800

    AI-3.4.1 <sunpc@SunPcdeMacBook-Pro.local Create IntelliLang.xml, myStyle.xml, myKey.xml, project.default.xml, diff.xml, equalsHashCodeTemplates.xml, Enum.java, Interface.java, Default.xml, ignore.xml, find.xml, File Header.java, StudioMy.xml, androidEditors.xml, gradle.run.settings.xml, AndroidComments.xml, code.style.schemes.xml, path.macros.xml, hg.xml, Default.xml, Mac OS X 10_5 my.xml, Activity.java, editor.codeinsight.xml, keymap.xml, editor.xml, markdown_navigator.shared.xml, debugger.xml, updates.xml, baseRefactoring.xml, ide.general.xml, Singleton.java, git.xml, other.xml, androidStudioFirstRun.xml, ui.lnf.xml, gradle.settings.xml, filetypes.xml, projectView.xml, plugin_ui.xml, colors.scheme.xml, android.gradle.studio.xml, github.xml, AnnotationType.java, customization.xml, studioFlags.xml, parameter.hints.xml, Class.java, jdk.table.xml, myConfig0126.icls, laf.xml, web-browsers.xml, proxy.settings.xml, vcs.xml, toStringTemplates.xml, usageView.xml

---
+++ fileTemplates/includes/File Header.java
@@ -0,0 +1,5 @@
+/**
+ * 
+ *
+ * @author by SunYuXing on ${DATE}.
+ */
---
+++ fileTemplates/j2ee/Activity.java
@@ -0,0 +1,12 @@
+package ${PACKAGE_NAME};
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+#parse("File Header.java")
+public class ${NAME} extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
